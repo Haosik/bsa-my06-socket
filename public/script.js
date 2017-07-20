007bb2214cca6d254a79d3ed98f9312f853aa46d
Haosik <noobbaster@gmail.com>
2017-07-20 10:45:06 +0300

    v 0.8.0

--- public/script.js
+++ public/script.js
@@ -1,136 +1,187 @@
-function chat() {
-	// DOM Variables
-	const modalBg = document.getElementById('modal-bg');
-	const nameInput = document.getElementById('name-input');
-	const nicknameInput = document.getElementById('nickname-input');
-	const submitNickname = document.getElementById('submit-nickname');
-	const nicknameForm = document.getElementById('nickname-form');
-	const nicknameModal = document.getElementById('nickname-modal');
+// DOM Variables
+const modalBg = document.getElementById('modal-bg');
+const nameInput = document.getElementById('name-input');
+const nicknameInput = document.getElementById('nickname-input');
+const submitNickname = document.getElementById('submit-nickname');
+const nicknameForm = document.getElementById('nickname-form');
+const nicknameModal = document.getElementById('nickname-modal');
 
-	const chatMessages = document.getElementById('chat-messages');
-	const usersList = document.getElementById('chat-users');
+const chatMessages = document.getElementById('chat-messages');
+const usersList = document.getElementById('chat-users');
 
-	const chatHeader = document.getElementById('chat-header');
-	const messageForm = document.getElementById('message-form');
-	const messageInput = document.getElementById('input-msg');
-	const submitMessage = document.getElementById('submit-message');
+const chatHeader = document.getElementById('chat-header');
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('input-msg');
+const submitMessage = document.getElementById('submit-message');
 
-	// User variables
-	let userName = '';
-	let userNick = '';
+// User variables
+let userName = '';
+let userNick = '';
+let connected = false;
+let typing = false;
+let lastTypingTime;
 
-	// Event listeners
-	nicknameInput.addEventListener("keydown", (e) => {
-		//No spaces allowed to be pressed
-		if (e.keyCode == 32) {
-			e.preventDefault();
-		}
-	});
-	nicknameForm.addEventListener("submit", function (e) {
+let socket = io();
+
+// Event listeners
+nicknameInput.addEventListener("keydown", (e) => {
+	//No spaces allowed to be pressed
+	if (e.keyCode == 32) {
 		e.preventDefault();
-		//At least 3 symbols for nick and no spaces
-		if (nicknameInput.value.length > 3 && !nicknameInput.value.match(/[* ]/g)) {
-			userName = nameInput.value;
-			userNick = nicknameInput.value;
-
-			modalBg.classList.add('js-hidden');
-			nicknameModal.classList.add('js-hidden');
-			chatHeader.innerHTML = `Hello, ${name} @${userNick}!`;
-			console.log(userNick);
-			ajaxRequest({
-				method: 'POST',
-				url: '/',
-				data: {userNick: userNick}
-			});
-		} else {
-			nicknameInput.classList.add('not-valid');
-		}
-	});
-
-	messageForm.addEventListener("submit", function (e) {
-		e.preventDefault();
-
-		let message = messageInput.value;
-
-		let data = {
-			userName,
-			userNick,
-			text: message
-		};
-
-		messageInput.value = "";
-
-		ajaxRequest({
-			method: 'POST',
-			url: '/messages',
-			data
-		})
-	});
-	
-	window.onbeforeunload = function() {
-		ajaxRequest({
-			method: 'DELETE',
-			url: '/',
-			data: {userNick}
-		})
-	};
-
-	// AJAX requests
-
-	let ajaxRequest = function (options) {
-		let url = options.url || '/';
-		let method = options.method || 'GET';
-		let callback = options.callback || function () {};
-		let data = options.data || {};
-		let xmlHttp = new XMLHttpRequest();
-
-		xmlHttp.open(method, url, true);
-		xmlHttp.setRequestHeader('Content-Type', 'application/json');
-		xmlHttp.send(JSON.stringify(data));
-
-		xmlHttp.onreadystatechange = function () {
-			if (xmlHttp.status === 200 && xmlHttp.readyState === 4) {
-				callback(xmlHttp.responseText);
-			}
-		};
-	};
-
-	let getData = function () {
-		ajaxRequest({
-			url: '/messages',
-			method: 'GET',
-			callback: function (respData) {
-				data = JSON.parse(respData);
-				chatMessages.innerHTML = "";
-				usersList.innerHTML = "";
-
-				msg = data.data;
-				users = data.usersInChat;
-				console.log(users);
-				for (let i in msg) {
-					let li = document.createElement('li');
-					li.innerHTML = `<span class="chat-msg__name">${msg[i].userName}</span>
-					 <span class="chat-msg__nick">@${msg[i].userNick}
-					  <span class="chat-msg__date">(${new Date(msg[i].date).toLocaleString()})
-					</span> <div class="chat-msg__text">${msg[i].text}</div>`;
-					li.classList.add('chat-msg__wrap');
-					if (msg[i].text.includes('@' + userNick)) {
-						li.classList.add('private-message')
-					}
-
-					chatMessages.appendChild(li);
-				}
-				for (let x in users) {
-					let userDiv = document.createElement('div');
-					userDiv.innerHTML = `<span>@${users[x]}</span>`;
-					userDiv.classList.add('user-div');
-					usersList.appendChild(userDiv);
-				}
-			}
-		})
 	}
-	getData();
+});
+nicknameForm.addEventListener("submit", function (e) {
+	e.preventDefault();
+	//At least 3 symbols for nick and no spaces
+	if (nicknameInput.value.length > 3 && !nicknameInput.value.match(/[* ]/g)) {
+		userName = nameInput.value;
+		userNick = nicknameInput.value;
 
-	setInterval(() => getData(), 1000);
-};
-chat();
+		modalBg.classList.add('js-hidden');
+		nicknameModal.classList.add('js-hidden');
+		chatHeader.innerHTML = `Hello, ${name} @${userNick}!`;
+		console.log(userNick);
+		// ajaxRequest({
+		// 	method: 'POST',
+		// 	url: '/',
+		// 	data: {userNick: userNick}
+		// });
+		socket.emit('add user', userNick);
+	} else {
+		nicknameInput.classList.add('not-valid');
+	}
+});
+
+messageForm.addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	let message = messageInput.value;
+
+	let data = {
+		userName,
+		userNick,
+		text: message,
+		date: Date.now()
+	};
+	socket.emit('chat message', data);
+
+	messageInput.value = "";
+
+	// ajaxRequest({
+	// 	method: 'POST',
+	// 	url: '/messages',
+	// 	data
+	// })
+});
+
+messageInput.oninput = (e) => {
+	socket.emit('typing', {
+		userNick
+	});
+	let typingTime = setTimeout(() => {
+		socket.emit('stop typing');
+	}, 1500);
+}
+
+socket.on('chat message', function (msg) {
+	let li = document.createElement('li');
+	li.innerHTML = `<span class="chat-msg__name">${msg.userName}</span>
+					 <span class="chat-msg__nick">@${msg.userNick}
+					  <span class="chat-msg__date">(${new Date(msg.date).toLocaleString()})
+					</span> <div class="chat-msg__text">${msg.text}</div>`;
+	li.classList.add('chat-msg__wrap');
+	if (msg.text.includes('@' + userNick)) {
+		li.classList.add('private-message')
+	}
+	chatMessages.appendChild(li);
+});
+
+// Adds the visual chat typing message
+function addChatTyping(data) {
+	let typingMessage = document.getElementById('typing-message');
+	typingMessage.innerText = `${data.userNick} is typing...`
+	// data.typing = true;
+	// data.message = 'is typing';
+	// addChatMessage(data);
+}
+// Removes the visual chat typing message
+function removeChatTyping() {
+	let typingMessage = document.getElementById('typing-message');
+	typingMessage.innerText = '';
+}
+// Whenever the server emits 'typing', show the typing message
+socket.on('typing', function (data) {
+	data = {
+		userNick
+	};
+	addChatTyping(data);
+});
+socket.on('stop typing', function (data) {
+	removeChatTyping();
+});
+
+// window.onbeforeunload = function() {
+// 	ajaxRequest({
+// 		method: 'DELETE',
+// 		url: '/',
+// 		data: {userNick}
+// 	})
+// };
+
+// AJAX requests
+
+// let ajaxRequest = function (options) {
+// 	let url = options.url || '/';
+// 	let method = options.method || 'GET';
+// 	let callback = options.callback || function () {};
+// 	let data = options.data || {};
+// 	let xmlHttp = new XMLHttpRequest();
+
+// 	xmlHttp.open(method, url, true);
+// 	xmlHttp.setRequestHeader('Content-Type', 'application/json');
+// 	xmlHttp.send(JSON.stringify(data));
+
+// 	xmlHttp.onreadystatechange = function () {
+// 		if (xmlHttp.status === 200 && xmlHttp.readyState === 4) {
+// 			callback(xmlHttp.responseText);
+// 		}
+// 	};
+// };
+
+// let getData = function () {
+// 	ajaxRequest({
+// 		url: '/messages',
+// 		method: 'GET',
+// 		callback: function (respData) {
+// 			data = JSON.parse(respData);
+// 			chatMessages.innerHTML = "";
+// 			usersList.innerHTML = "";
+
+// 			msg = data.data;
+// 			users = data.usersInChat;
+// 			console.log(users);
+// 			for (let i in msg) {
+// 				let li = document.createElement('li');
+// 				li.innerHTML = `<span class="chat-msg__name">${msg[i].userName}</span>
+// 				 <span class="chat-msg__nick">@${msg[i].userNick}
+// 				  <span class="chat-msg__date">(${new Date(msg[i].date).toLocaleString()})
+// 				</span> <div class="chat-msg__text">${msg[i].text}</div>`;
+// 				li.classList.add('chat-msg__wrap');
+// 				if (msg[i].text.includes('@' + userNick)) {
+// 					li.classList.add('private-message')
+// 				}
+
+// chatMessages.appendChild(li);
+// 			}
+// 			for (let x in users) {
+// 				let userDiv = document.createElement('div');
+// 				userDiv.innerHTML = `<span>@${users[x]}</span>`;
+// 				userDiv.classList.add('user-div');
+// 				usersList.appendChild(userDiv);
+// 			}
+// 		}
+// 	})
+// }
+// getData();
+
+// setInterval(() => getData(), 1000);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// const express = require('express');
 const express = require('express');
-const app = express();
-var bodyParser = require('body-parser');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const path = require('path');
+var bodyParser = require('body-parser');
+
 const port = 3001;
 
 const mongoose = require('mongoose');
@@ -9,70 +14,123 @@ mongoose.connect('mongodb://localhost/chatmessages');
 mongoose.set("debug", true);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to Mongo!');
+db.once('open', function () {
+	console.log('Connected to Mongo!');
 });
 
 const messageService = require('./services/message');
 
 let usersInChat = [];
+let numUsers = 0;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 
-
-
+app.use(express.static('public'));
 app.get('/', (req, res, next) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.use(express.static('public'));
+//Socket CONNECTED
+io.on('connection', (socket) => {
+	let addedUser = false;
+	console.log('a user connected!');
 
-app.post('/', (req, res, next) => {
-	console.log(req.body.userNick);
-	usersInChat.push(req.body.userNick);
-	res.send();
-});
+	//CHAT MESSAGE 
+	socket.on('chat message', function (msg) {
+		io.emit('chat message', msg);
+	});
 
-app.delete('/', (req, res, next) => {
-	console.log(req.body.userNick);
-	usersInChat.splice(usersInChat.indexOf(req.body.userNick), 1);
-	// res.send();
-});
+	// when the client emits 'add user', this listens and executes
+	socket.on('add user', function (username) {
+		if (addedUser) return;
+
+		// we store the username in the socket session for this client
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('user joined', {
+			username: socket.userName,
+			usernick: socket.adapter.userNick,
+			numUsers: numUsers
+		});
+	});
+
+	// when the client emits 'typing', we broadcast it to others
+	socket.on('typing', function () {
+		socket.broadcast.emit('typing', {
+			usernick: socket.userNick
+		});
+	});
+
+	// when the client emits 'stop typing', we broadcast it to others
+	socket.on('stop typing', function () {
+		socket.broadcast.emit('stop typing', {
+			usernick: socket.userNick
+		});
+	});
+
+	//Socket DISCONNECTED
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+})
+
+// app.post('/', (req, res, next) => {
+// 	console.log(req.body.userNick);
+// 	usersInChat.push(req.body.userNick);
+// 	res.send();
+// });
+
+// app.delete('/', (req, res, next) => {
+// 	console.log(req.body.userNick);
+// 	usersInChat.splice(usersInChat.indexOf(req.body.userNick), 1);
+// 	// res.send();
+// });
 
 
-app.get('/messages', (req, res, next) => {
-	messageService.findAllMessages((err, data) => {
-		if (!err) {
-			let respData = {
-				data,
-				usersInChat
-			}
-			res.send(JSON.stringify(respData));
-			return
-		}
-		next(err);
-	})
-});
+// app.get('/messages', (req, res, next) => {
+// 	messageService.findAllMessages((err, data) => {
+// 		if (!err) {
+// 			let respData = {
+// 				data,
+// 				usersInChat
+// 			}
+// 			res.send(JSON.stringify(respData));
+// 			return
+// 		}
+// 		next(err);
+// 	})
+// });
 
-app.post('/messages', (req, res, next) => {
-	messageService.saveMessage(req.body, (err, data) => {
-		if (!err) {
-			res.send(200);
-			return
-		}
-		next(err);
-	})
-});
+// app.post('/messages', (req, res, next) => {
+// 	messageService.saveMessage(req.body, (err, data) => {
+// 		if (!err) {
+// 			res.send(200);
+// 			return
+// 		}
+// 		next(err);
+// 	})
+// });
 
 
 
 app.use((req, res, next) => {
-		res.status(404);
+	res.status(404);
 });
 app.use((err, req, res, next) => {
 	console.log('500', err);
 	res.status(500);
 });
 
-app.listen(port);
+// app.listen(port);
+http.listen(port, () => {
+	console.log(`Server started. Listening on port ${port}`);
+})
